044fdc576e80be996d1280745c85d21a54b22e69
Arsalan Kazmi <sonicspeed848@gmail.com>
2024-03-11 19:21:18 +0000

    Clamped health value to health bar max
    I found out that the reason Joel wasn't consistently taking damage is that he was getting overhealed. So this fixes that.

--- Scripts/JoelMovements.gd
+++ Scripts/JoelMovements.gd
@@ -123,6 +123,7 @@ func _process(delta):
 	if style_duration <= 0 and style_rank > StyleRank.None:
 		style_duration = style_rank_meter.max_value - 1
 		style_rank -= 1
+	health = clamp(health, 0, health_bar.max_value)
 	if health_bar.value <= 0:
 		get_tree().change_scene_to_file("res://death_screen.tscn")
 	if Input.is_action_just_pressed("Menu"):
